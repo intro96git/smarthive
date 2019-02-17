2103acbe33cef5cbb5fa0d5e78b7ddd0efd98aa0
Mike Intravaia <intro96@gmail.com>
2019-02-17 14:02:26 -0500

    Adjusted Vagrant provisioning

--- provision.sh
+++ provision.sh
@@ -2,7 +2,11 @@
 
 pacman -Syu --noconfirm
 pacman -S --noconfirm extra/jdk-openjdk scala scala-docs scala-sources sbt git docker
+if [ -f /vagrant/.bashrc ]; then
+	cat /vagrant/.bashrc >> /home/vagrant/.bashrc
+fi
 cat >>/home/vagrant/.bashrc <<EOF
 export PATH=/vagrant:$PATH
 EOF
-shutdown --reboot
+echo ==== Run 'vagrant up' again to start using the machine ====
+shutdown --halt
