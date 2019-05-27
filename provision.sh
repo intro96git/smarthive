6a67b7b20bc5cd0a2886696b4c159cd4ce91457a
Mike Intravaia <intro96@gmail.com>
2019-05-26 23:52:43 -0400

    Provision Vagrant box with Ammonite

--- provision.sh
+++ provision.sh
@@ -5,6 +5,7 @@ pacman -S --noconfirm extra/jdk-openjdk scala scala-docs scala-sources sbt git d
 if [ -f /vagrant/.bashrc ]; then
 	cat /vagrant/.bashrc >> /home/vagrant/.bashrc
 fi
+curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.6.7/2.12-1.6.7 > /usr/local/bin/amm && chmod +x /usr/local/bin/amm
 cat >>/home/vagrant/.bashrc <<EOF
 export PATH=/vagrant:$PATH
 EOF
