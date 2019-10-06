68aa8cbfc88990d824fe7b6efeea4789a49da67c
Mike Intravaia <intro96@gmail.com>
2019-10-05 21:09:26 -0400

    Added Bloop for later use

--- provision.sh
+++ provision.sh
@@ -1,14 +1,30 @@
 #!/bin/sh
 
 pacman -Syu --noconfirm
-pacman -S --noconfirm extra/jdk-openjdk scala scala-docs scala-sources sbt git docker 
+
+# binutils, meson, gtest, gmock, cmake, pkg-config, fakeroot, and gcc needed to build and use AUR tool (auracle)
+pacman -S --noconfirm extra/jdk-openjdk scala scala-docs scala-sources sbt git docker binutils meson gtest gmock gcc pkg-config cmake fakeroot
+
+cd /tmp
+sudo -u vagrant -- git clone https://aur.archlinux.org/auracle-git.git
+cd auracle-git/
+sudo -u vagrant -- makepkg -iCr --noconfirm
+cd ..
+rm -rf auracle-git
+sudo -u vagrant -- auracle clone bloop
+cd bloop
+sudo -u vagrant -- makepkg -iCr --noconfirm
+cd ..
+rm -rf bloop
+
+sudo -u vagrant -- systemctl --user enable bloop
+
 if [ -f /vagrant/.bashrc ]; then
 	cat /vagrant/.bashrc >> /home/vagrant/.bashrc
 fi
-curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.6.7/2.12-1.6.7 > /usr/local/bin/amm && chmod +x /usr/local/bin/amm
+curl -L https://github.com/lihaoyi/Ammonite/releases/download/1.7.4/2.12-1.7.4 > /usr/local/bin/amm && chmod +x /usr/local/bin/amm
 cat >>/home/vagrant/.bashrc <<EOF
 export PATH=/vagrant:$PATH
 EOF
 echo ==== Run 'vagrant up' again to start using the machine ====
-echo ==== Run 'vagrant rsync-auto >/dev/null &' before working w/ VM to make sure files stay in sync ====
 poweroff
--- sbt
+++ sbt
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#TERM=xterm-color JAVA_OPTS=-Xms2G /usr/bin/sbt -Dsbt.ivy.home=/vagrant/.ivy2/ -Divy.home=/vagrant/.ivy2/ "$@"
 TERM=xterm-color /usr/bin/sbt -Dsbt.ivy.home=/vagrant/.ivy2/ -Divy.home=/vagrant/.ivy2/ -mem 2048 "$@"
